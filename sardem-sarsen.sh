71727ad05b3d76c9f8105d200da053b3bc15749d
Sujen Shah <sujen1412@users.noreply.github.com>
2025-09-04 12:48:08 -0700

    Update sardem-sarsen.sh to include basedir

--- sardem-sarsen.sh
+++ sardem-sarsen.sh
@@ -7,13 +7,13 @@ export LANG=C.UTF-8
 # NASA DPS command-line is --bbox ${BBOX} --stac_catalog_folder ${CATALOG_PATH}
 # ${BBOX} : bounding box coordinate
 # ${CATALOG_PATH} : path into a STAC Catalog
-
+basedir=$( cd "$(dirname "$0")" ; pwd -P)
 # Creating output folder
 outdir="output"
 mkdir -p "${outdir}"
 
 # shellcheck disable=SC2068
-conda run --live-stream -n dem python sardem-sarsen/sardem-sarsen.py $@ -o "${outdir}"
+conda run --live-stream -n dem python ${basedir}/sardem-sarsen.py $@ -o "${outdir}"
 
 # print output dir for debug
 find "${outdir}" -type f
